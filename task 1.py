7bb69eb3309fe9052e691f2513e0e972d4f4ec17
sohandevper <sohanreddy1547@gmail.com>
2025-12-19 14:52:39 +0300

    final commit

---
+++ task 1.py
@@ -0,0 +1,13 @@
+
+num1= int(input('Enter your first number: '))
+num2= int(input('Enter your second number: '))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f'Addition: {addition}')
+print(f'Subtraction: {subtraction}')
+print(f'Multiplication: {multiplication}')
+print(f'Division: {division}')
